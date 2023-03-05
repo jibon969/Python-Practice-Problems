79b44e266f20d4b8ecf37c00de68ea190f673475
jayed969 <jayed.swe@gmail.com>
2023-03-05 21:42:25 +0600

    current date and time

---
+++ Python-Basic/01- current-date-and-time.py
@@ -0,0 +1,21 @@
+
+from datetime import datetime
+
+date_time = datetime.today()
+current_date_time = date_time.strftime("%y-%m-%d %H:%M:%S")
+"""
+2. %y-%m-%d  = year, month, date
+1. %H:%M%S    = Hour, Minute, Second
+"""
+print("current date time : ", current_date_time)
+print("\n")
+
+
+def current_date_time():
+    today_date = datetime.today()
+    current_time = today_date.strftime("%y-%m-%d %H:%M:%S")
+    return current_time
+
+
+result = current_date_time()
+print("current date time : ", result)

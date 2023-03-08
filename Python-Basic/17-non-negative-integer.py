038caaf1bb7011bbae0957807bec2416eadcecf8
jayed969 <jayed.swe@gmail.com>
2023-03-08 19:39:55 +0600

    added unchanged string & not integer

---
+++ Python-Basic/17-non-negative-integer.py
@@ -0,0 +1,9 @@
+"""
+Write a Python program that returns a string that is n (non-negative integer) copies of a given string.
+"""
+
+value = "3"
+if int(value) < 0:
+    print("Invalid input")
+else:
+    print(value)

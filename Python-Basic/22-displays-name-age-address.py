f3c668ed2aeafec73e5d20e659d06db5ff3f7d6b
jayed969 <jayed.swe@gmail.com>
2023-03-10 23:24:37 +0600

    added display data

---
+++ Python-Basic/22-displays-name-age-address.py
@@ -0,0 +1,13 @@
+"""
+Write a Python program that displays your name, age, and address on three different lines
+"""
+
+
+def display_name(name, age, address):
+    print(name)
+    print(age)
+    print(address)
+
+
+display_name("Jibon", 23, "Dhaka, Bangladesh")
+

6e0f0ce90e830612584c9412008fc10e510ec4d1
jayed969 <jayed.swe@gmail.com>
2023-03-11 09:08:59 +0600

    added height of triangle

---
+++ Python-Basic/26-triangle-height.py
@@ -0,0 +1,15 @@
+"""
+ Write a Python program that will accept the base and height of a 
+ triangle and compute its area.
+"""
+base = 20
+height = 40
+triangle = 1/2 * base * height
+print(triangle)
+
+
+# user input
+b = int(input("Enter base number : "))
+h = int(input("Enter base height : "))
+area = 1/2 * b*h
+print(area)

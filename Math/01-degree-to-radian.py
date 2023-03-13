bc9a67c56915b61f45f1ef641f9b2810e148cf34
jayed969 <jayed.swe@gmail.com>
2023-03-13 21:06:37 +0600

    added math

---
+++ Math/01-degree-to-radian.py
@@ -0,0 +1,16 @@
+
+"""
+Write a Python program to convert degree to radian.
+"""
+
+import math
+
+degree = int(input("Enter a degree number : "))
+pi = math.pi
+result = degree * (pi/180)
+print('Convert degree to radian : ', result)
+print('\n')
+
+
+
+

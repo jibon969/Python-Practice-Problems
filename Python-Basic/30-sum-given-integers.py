b99c1912826a45f8266612734355cbf5d966290d
jayed969 <jayed.swe@gmail.com>
2023-03-15 21:59:17 +0600

    added sum-given number

---
+++ Python-Basic/30-sum-given-integers.py
@@ -0,0 +1,29 @@
+"""
+Write a Python program to sum three given integers. However, if two values are equal, the sum will be zero.
+
+case 1 : sum three given integers
+case 2 : if two values are equal, the sum will be zero.
+"""
+
+a = 1
+b = 1
+c = 1
+
+d = a + b + c
+
+if a == b or b == c or a == c:
+    print("Zero")
+else:
+    print(d)
+
+
+def three_integer(a, b, c):
+    if a == b or b == c or a == c:
+        x = "Zero"
+        return x
+    else:
+        return a + b + c
+
+
+d = three_integer(20, 60, 7)
+# print(d)

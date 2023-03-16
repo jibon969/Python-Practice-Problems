2f0802199cc93dce9691ce8f094ff80ff7dadeb2
jayed969 <jayed.swe@gmail.com>
2023-03-16 21:31:27 +0600

    added name, age, and address three lines

---
+++ Python-Basic/35- three-different-lines.py
@@ -0,0 +1,20 @@
+"""
+Write a Python program that displays your name, age, and address on three different lines
+"""
+
+name = "jibon"
+age = 28
+address = "69/A, Dhaka"
+print(name)
+print(age)
+print(address)
+
+
+def personal_info(name, age, address):
+    print(name)
+    print(age)
+    print(address)
+
+
+result = personal_info("Jibon", 20, "69/A")
+print(result)

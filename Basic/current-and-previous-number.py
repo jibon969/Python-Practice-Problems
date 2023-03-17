3227bc48b1877374bd13722f42cf22135a2db155
jayed969 <jayed.swe@gmail.com>
2023-03-17 21:21:06 +0600

    added current and previous number

---
+++ Basic/current-and-previous-number.py
@@ -0,0 +1,12 @@
+"""
+Write a program to iterate the first 10 numbers and in each iteration,
+print the sum of the current and previous number.
+"""
+
+previous_number = 0
+
+for i in range(1, 11):
+    x = previous_number + i
+    print("Current Number", i, "Previous Number", previous_number, "Sum", preNumber + i)
+
+    previous_number = i

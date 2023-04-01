36b42545357fbbb1001b56ce2159b5e87b2e57a8
jayed969 <jayed.swe@gmail.com>
2023-04-01 17:02:54 +0600

    aded maximum value

---
+++ GeeksforGeeks/Basic/02-maximum-of-two-numbers.py
@@ -0,0 +1,43 @@
+a = -2
+b = -4
+
+output = max(a, b)
+print(output)
+
+# maximum
+
+
+def maximum(a, b):
+    if a >= b:
+        return a
+    else:
+        return b
+
+output = maximum(-50, -34)
+print(output)
+
+
+# Use of ternary operator
+a = 2
+b = 4
+print(a if a >= b else b)
+
+
+# Using lambda function
+maximum = lambda a, b: a if a >= b else b
+print(maximum(3, 8))
+
+
+# Using list comprehension
+a = 2
+b = 4
+x = [a if a > b else b]
+print(x)
+
+
+# maximum of two numbers
+a = 12
+b = -4
+x =[a,b]
+x.sort()
+print(x[1])

1bbf39373c617576ce25f8b1b84eb19164c3ecdd
jayed969 <jayed.swe@gmail.com>
2023-04-08 14:06:32 +0600

    added len of list and covert strint to int, float

---
+++ GeeksforGeeks/Basic/05-length-of-list.py
@@ -0,0 +1,8 @@
+"""
+Demonstrating finding length of a list using the Naive Method
+"""
+
+# Initializing list
+test_list = [1, 4, 5, 7, 8]
+result = len(test_list)
+print(result)
---
+++ Python-Basic/41-Parse-string-Float-Integer.py
@@ -0,0 +1,9 @@
+"""
+Parse a string to Float or Integer
+"""
+
+a = "23.45"
+b = float(a)
+print(b)
+c = int(float(a))
+print(c)

52f822179a2329c0adefdfd1c3bc6b61be28a6c7
jayed969 <jayed.swe@gmail.com>
2023-04-08 23:25:37 +0600

    added hypotenuse of a right angled triangle.

---
+++ Python-Basic/45-hypotenuse-triangle.py
@@ -0,0 +1,11 @@
+
+"""
+Write a Python program to calculate the hypotenuse of a right angled triangle.
+"""
+
+a = int(input("Enter 1st number : "))
+b = int(input("Enter 2nd number : "))
+
+result = (a**2 + b**2)/.5
+print(result)
+

9596a9b85923ea3a5c320f5a58eb36ab2ede9aea
jayed969 <jayed.swe@gmail.com>
2023-04-08 23:22:20 +0600

    added convert convert height (in feet and inches) to centimeters

---
+++ Python-Basic/44-feet-inches-centimeters.py
@@ -0,0 +1,12 @@
+
+"""
+Write a Python program to convert height (in feet and inches) to centimeters.
+"""
+
+feet = 3.5
+
+inches = feet * 12
+print(inches)
+
+centimeter = inches * 2.54
+print(centimeter)

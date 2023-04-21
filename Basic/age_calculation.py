3f68c4a2069613e09d648adb6724e9f466510b55
jayed969 <jayed.swe@gmail.com>
2023-04-21 22:04:25 +0600

    change files

--- Basic/age_calculation.py
+++ Basic/age_calculation.py
@@ -1,5 +1,5 @@
-user_input = input("Enter your Age : ")
 import datetime
+user_input = input("Enter your Age : ")
 Age = datetime.date.today().year - int(user_input)
 print("You Are " + str(Age) + " years old")
 print('\n')

fb4644b4cb39eb34f519358df2a87d66cbcf7d82
jayed969 <jayed.swe@gmail.com>
2023-04-26 22:20:10 +0600

    added some line indentation

--- Python-Basic/33-check-values.py
+++ Python-Basic/33-check-values.py
@@ -1,5 +1,6 @@
 """
-Write a Python program that returns true if the two given integer values are equal or their sum or difference is 5
+Write a Python program that returns true if the two given
+integer values are equal or their sum or difference is 5
 
 """
 
--- Python-Basic/37-calculate-the-distance.py
+++ Python-Basic/37-calculate-the-distance.py
@@ -1,5 +1,6 @@
 """
-Write a Python program to calculate the distance between the points (x1, y1) and (x2, y2).
+Write a Python program to calculate the distance
+between the points (x1, y1) and (x2, y2).
 case : distance (x1, y1)
 case : distance (x2, y2)
 

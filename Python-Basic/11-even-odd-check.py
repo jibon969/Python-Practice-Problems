73b7a9c03d3dbf4a55fd8e46f294b883dd617bb2
jayed969 <jayed.swe@gmail.com>
2023-04-27 22:44:00 +0600

    added some docs string and others

--- Python-Basic/11-even-odd-check.py
+++ Python-Basic/11-even-odd-check.py
@@ -1,6 +1,7 @@
 """
 Write a Python program that determines whether a given number
-(accepted from the user) is even or odd, and prints an appropriate message to the user.
+(accepted from the user) is even or odd,
+and prints an appropriate message to the user.
 """
 
 user = int(input("Enter your number : "))

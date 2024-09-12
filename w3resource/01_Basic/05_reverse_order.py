94ae2ff3e60871106dfca6701ed79e03688caca3
jibontech <jibon.belaface@gmail.com>
2024-09-12 10:28:14 +0600

    added prints them in reverse order

---
+++ w3resource/01_Basic/05_reverse_order.py
@@ -0,0 +1,15 @@
+"""
+Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them.
+"""
+
+# Output 
+"""
+Input  : Jibon Ahmed
+output : Ahmed Jibon
+"""
+
+first_name = "Jibon"
+last_name = "Ahmed"
+
+reverse_order = last_name + " " + first_name
+print(reverse_order)

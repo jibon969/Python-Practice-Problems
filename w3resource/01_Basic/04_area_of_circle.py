37fd4f260531e68e3e393fd7706e5c4227788c76
jibontech <jibon.belaface@gmail.com>
2024-09-12 10:21:13 +0600

    added area of a circle

---
+++ w3resource/01_Basic/04_area_of_circle.py
@@ -0,0 +1,16 @@
+"""
+Write a Python program that calculates the area of a circle based on the radius entered by the user.
+"""
+
+# Output 
+"""
+Sample Output :
+r = 1.1
+Area = 3.8013271108436504
+"""
+from math import pi
+
+area  = 3.8013271108436504
+area_of_circle = pi * area ** 2
+print(area_of_circle)
+

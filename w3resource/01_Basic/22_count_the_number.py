e342bf46597e2a6fc1961c863d0fbfed17734a1b
jibontech <jibon.belaface@gmail.com>
2024-09-26 09:49:03 +0600

    added copy string from list

--- w3resource/01_Basic/22_count_the_number.py
+++ w3resource/01_Basic/22_count_the_number.py
@@ -2,7 +2,7 @@
 Write a Python program to count the number 4 in a given list.
 """
 
-list_count_4 = [1, 4, 6, 7, 4]
+list_count_4 = [1, 4, 6, 7, 4, 4]
 count = 0
 for i in list_count_4:
     if i == 4:
